0b34bc768ae32c1915d5e1ca15a45624dfa5d668
namerajendrasingh <rajendra.singh068@gmail.com>
2025-11-05 08:08:00 +0530

    new changes

--- src/com/raj/core/Test.java
+++ src/com/raj/core/Test.java
@@ -1,9 +1,12 @@
 package com.raj.core;
 
 public class Test {
-
 	public static void main(String[] args) {
 		System.out.println("Hello Java 17");
+		String str = """
+				     <html><head></head>
+				      <body><h1>Hello We are working with Java 17</body>
+				     </html>
+				     """;
 	}
-
 }
